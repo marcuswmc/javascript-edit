f71cd9e693f18c974d3c2204239032101ad3927d
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2024-05-23 14:16:13 +0100

    feat: Exercicios Aula 7

--- class-7/script.js
+++ class-7/script.js
@@ -1,22 +1,50 @@
-const soma = (a, b) => {
-  const resultado = a + b
+// const soma = (a, b) => {
+//   const resultado = a + b
 
-  return new Promise((resolve, reject) => {
-    if(resultado % 2 === 0) {
-      resolve(`A soma é par: ${resultado}`)
-    } else {
-      reject(`A soma é impar: ${resultado}`)
-    }
-  }) 
+//   return new Promise((resolve, reject) => {
+//     if(resultado % 2 === 0) {
+//       resolve(`A soma é par: ${resultado}`)
+//     } else {
+//       reject(`A soma é impar: ${resultado}`)
+//     }
+//   }) 
+// }
+
+// async function results(a, b) {
+//   try {
+//     const result = await soma(a, b)
+//     console.log(result)
+//   } catch(e) {
+//     console.log(e)
+//   }  
+// }
+
+// results(5,2)
+
+
+const name = (a) => {
+  const promise = new Promise((resolve, reject) => {
+    if(a != "javascript"){
+      resolve(a)
+    } else (
+      reject("Nome não é um nome")
+    )
+  })
+  
+  promise
+  .then((result) => console.log(result)).catch((err) => console.log(err))
 }
 
-async function results(a, b) {
+name('Marcus')
+
+// ASYNC VERSION
+const asyncNome = async (a) => {
   try {
-    const result = await soma(a, b)
+    const result = await name(a)
     console.log(result)
   } catch(e) {
     console.log(e)
-  }  
+  }
 }
 
-results(5,2)
+asyncNome("javascript")
